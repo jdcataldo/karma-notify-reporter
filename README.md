e383b8b43437615cfa4c91b7c80f25793dd16f49
Justin Cataldo <justin.cataldo@animoto.com>
2014-07-16 08:34:04 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# karma-notify-reporter
+
+> Report test results using OSX Notification Center, [Growl](http://growl.info/) or notify-send.
+
+Built on top of [node-notifier](https://github.com/mikaelbr/node-notifier).  
+
+By default Notification Center will be used on Mac, notify-send will be used on Linux, and Growl will be used if neither Mac 10.8 or Linux.
+
+## Installation 
+
+```js
+npm install karma-notify-reporter --save-dev
+```
+
+###
+
+## Configuration
+```js
+// karma.conf.js
+module.exports = function(config) {
+  config.set({
+    reporters: ['progress', 'notify'],
+
+    // Optional Settings
+    notifyReporter: {
+      reportEachFailure: true, // Default: false, Will notify on every failed sepc
+      reportSuccess: false, // Default: true, Will notify when a suite was successful
+    }
+  });
+};
+```
+
+You can pass list of reporters as a CLI argument too:
+```bash
+karma start --reporters notify,dots
+```
---
+++ index.js
@@ -0,0 +1,84 @@
+var Notification = require('node-notifier'),
+    util         = require('util'),
+    path         = require('path');
+
+var messages = {
+  success : {
+    displayName : 'Success',
+    title       : 'PASSED - %s',
+    message     : '%d tests passed in %s.',
+    icon        : path.join(__dirname, 'images/success.png')
+  },
+  failed : {
+    displayName : 'Failure',
+    title       : 'FAILED - %s',
+    message     : '%d/%d tests failed in %s.',
+    icon        : path.join(__dirname, 'images/failed.png')
+  },
+  error : {
+    displayName : 'Aborted',
+    title       : 'ERROR - %s',
+    message     : '',
+    icon        : path.join(__dirname, 'images/error.png')
+  },
+  specFailed : {
+    displayName : 'Spec Failure',
+    title       : 'Spec Failed - %s',
+    message     : '%s',
+    icon        : path.join(__dirname, 'images/failed.png')
+  }
+};
+
+
+var NotifyReporter = function(baseReporterDecorator, helper, logger, config, formatError) {
+  var log               = logger.create('reporter.notify'),
+      reporterConfig    = config.notifyReporter || {},
+      reportSuccess     = typeof reporterConfig.reportSuccess !==  'undefined' ? reporterConfig.reportSuccess : true,
+      reportEachFailure = typeof reporterConfig.reportEachFailure !==  'undefined' ? reporterConfig.reportEachFailure : true,
+      notifier          = new Notification(),
+      msg;
+  
+  baseReporterDecorator(this);
+  
+  this.adapters = [];
+
+  this.onBrowserComplete = function(browser) {
+    var results = browser.lastResult,
+        time    = helper.formatTimeInterval(results.totalTime);
+
+    if (results.disconnected || results.error) {
+      msg = messages.error;
+      return notifier.notify(helper.merge(messages.error, {'title': util.format(msg.title, browser.name)}));
+    }
+
+    if (results.failed) {
+      msg = messages.failed;
+      return notifier.notify(helper.merge(msg, {'message': util.format(msg.message, results.failed, results.total, time), 'title': util.format(msg.title, browser.name)}));
+    }
+
+    if(reportSuccess) {
+      msg = messages.success;
+      notifier.notify(helper.merge(msg, {'message': util.format(msg.message, results.success, time), 'title': util.format(msg.title, browser.name)}));
+    }
+  };
+
+  if (reporterConfig.reportEachFailure){
+    this.specFailure = function(browser, result) {
+      var specName = result.suite.join(' ') + ' ' + result.description,
+          message  = util.format('%s: FAILED\n', specName);
+
+      result.log.forEach(function(log) {
+        message += formatError(log, '\t');
+      });
+
+      msg = messages.specFailed;
+      notifier.notify(helper.merge(msg, {'message': util.format(msg.message, message), 'title': util.format(msg.title, browser.name)}));
+    };
+  }
+};
+
+NotifyReporter.$inject = ['baseReporterDecorator', 'helper', 'logger', 'config', 'formatError'];
+
+module.exports = {
+  'reporter:notify': ['type', NotifyReporter]
+};
